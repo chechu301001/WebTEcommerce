7f916a3e57a4a0905b028347c720c0163f23f35d
Shreyas B Rao <chechu301001@gmail.com>
2022-12-05 22:35:29 +0530

    init-commit

--- server/routes/product.js
+++ server/routes/product.js
@@ -79,6 +79,7 @@ router.get("/", async (req, res) => {
     } else {
       products = await Product.find();
     }
+    console.log(products);
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
